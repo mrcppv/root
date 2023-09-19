e146a9c88f0477d9bdd32310a162837a4ce7d2fa
mrcppv <135101468+mrcppv@users.noreply.github.com>
2023-09-20 01:25:17 +0300

    Add files via upload

--- src/model/ToDo.js
+++ src/model/ToDo.js
@@ -1,36 +1,14 @@
 import ToDoItem from "./ToDoItem";
 
 class ToDo {
-    
+
     constructor() {
         this.items = this.loadFromLocalStorage();
     }
 
     add(item) {
         this.items.push(item);
-
-        // Previous way to save item to LocalStorage
-        //this.saveToLocalStorage();
-
-        // Create XMLHTTPRequest Object (AJAX Request)
-        var xhttp = new XMLHttpRequest();
-        // Add Callback function to the Request
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                // Check is item saved or not saved
-                if(this.responseText === "Saved"){
-                    console.log("Item Saved");
-                }else{
-                    console.log("Item Not Saved");
-                }
-            }
-          };
-        // Open Request
-        xhttp.open("POST", "http://localhost/vue/api/save.php", true);
-        // Set Header to be POST type
-        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        // Send Request with stringified Object
-        xhttp.send("item=" + JSON.stringify(item));
+        this.saveToLocalStorage();
     }
 
     delete(item) {
@@ -44,70 +22,24 @@ class ToDo {
     }
 
     saveToLocalStorage() {
-        // Previous way to save to LocalStorage of all items
-        //localStorage.setItem('todo-data', JSON.stringify(this.items));
-
-        // Create XMLHTTPRequest Object (AJAX Request)
-        var xhttp = new XMLHttpRequest();
-        // Add Callback function to the Request
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                // Check is item saved or not saved
-                if(this.responseText === "Saved"){
-                    console.log("Items Saved");
-                }else{
-                    console.log("Items Not Saved");
-                }
-            }
-          };
-        // Open Request
-        xhttp.open("POST", "http://localhost/vue/api/saveAll.php", false);
-        // Set Header to be POST type
-        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        // Send Request with stringified Object
-        xhttp.send("items=" + JSON.stringify(this.items));
+        localStorage.setItem('todo-data', JSON.stringify(this.items));
     }
 
     loadFromLocalStorage() {
-        // Previous way to load from LocalStorage
-        //var json = localStorage.getItem('todo-data');
-        
-        // Variable which will hold the data from API
-        var json;
-        // Create XMLHTTPRequest Object (AJAX Request)
-        var xhttp = new XMLHttpRequest();
-        // Open Request with third paramater value of false, because we want for script to wait for execution
-        xhttp.open("POST", "http://localhost/vue/api/load.php", false);
-        // Set Header to be POST type
-        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        // Add Callback function to the Request
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                // Load the items
-                json = this.responseText;
-            }
-        };
-        // Send the Request
-        xhttp.send();
+        var json = localStorage.getItem('todo-data');
 
-        // Changed this to match empty string instead of null, because we are saving this now to the file
-        if (json == ""){
+        if (json === null)
             return [];
-        }
-        
 
-        var jsonParsed = JSON.parse(json, (key, value) => {
+        let jsonParsed = JSON.parse(json, (key, value) => {
             if (key === "date") {
                 value = new Date(value);
             }
             return value;
         });
 
-        if (jsonParsed.length === 0){
+        if (jsonParsed.length === 0)
             return [];
-        }
-
-        
 
         let toDoItems = [];
 
